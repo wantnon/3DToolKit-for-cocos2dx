bc5dac11c8071742da024ec1287e153592f443c6
wantnon <350479720@qq.com>
2014-01-29 18:58:55 +0800

    change inverse matrix implementation to a better one.

--- HelloCpp/c3dToolKit/c3dMath.cpp
+++ HelloCpp/c3dToolKit/c3dMath.cpp
@@ -73,74 +73,141 @@ Cc3dMatrix4 transpose(const Cc3dMatrix4&mat){
     return Cc3dMatrix4(rs);
 }
 Cc3dMatrix4 inverse(const Cc3dMatrix4&mat){
-    float src[16];
-    for(int i=0;i<16;i++)src[i]=mat.getAt(i);
-    float inverse[16]={1,0,0,0,//col 1
-        0,1,0,0,
-        0,0,1,0,
-        0,0,0,1};
-    //----inverse src
-    int i, j, k, swap;
-    float t;
-    float temp[4][4];
+    //this code is copy from:http://stackoverflow.com/questions/1148309/inverting-a-4x4-matrix
     
-    for (i=0; i<4; i++) {
-        for (j=0; j<4; j++) {
-            temp[i][j] = src[i*4+j];
-        }
-    }
+    float m[16];
+    for(int i=0;i<16;i++)m[i]=mat.getAt(i);
     
-    for (i = 0; i < 4; i++) {
-        //
-        // Look for largest element in column
-        //
-        swap = i;
-        for (j = i + 1; j < 4; j++) {
-            if (fabsf(temp[j][i]) > fabsf(temp[i][i])) {
-                swap = j;
-            }
-        }
-        
-        if (swap != i) {
-            //
-            // Swap rows.
-            //
-            for (k = 0; k < 4; k++) {
-                t = temp[i][k];
-                temp[i][k] = temp[swap][k];
-                temp[swap][k] = t;
-                
-                t = inverse[i*4+k];
-                inverse[i*4+k] = inverse[swap*4+k];
-                inverse[swap*4+k] = t;
-            }
-        }
-        
-        if (temp[i][i] == 0) {
-            //No non-zero pivot.  The matrix is singular, which shouldn't
-            // happen.  This means the user gave us a bad matrix.
-            assert(false);
-            //return false;
-        }
-        
-        t = temp[i][i];
-        for (k = 0; k < 4; k++) {
-            temp[i][k] /= t;
-            inverse[i*4+k] /= t;
-        }
-        for (j = 0; j < 4; j++) {
-            if (j != i) {
-                t = temp[j][i];
-                for (k = 0; k < 4; k++) {
-                    temp[j][k] -= temp[i][k]*t;
-                    inverse[j*4+k] -= inverse[i*4+k]*t;
-                }
-            }
-        }
-    }//got inverse
-    Cc3dMatrix4 rs(inverse);
-    return rs;
+    float invOut[16];
     
+    float inv[16], det;
+    int i;
+    
+    inv[0] = m[5]  * m[10] * m[15] -
+    m[5]  * m[11] * m[14] -
+    m[9]  * m[6]  * m[15] +
+    m[9]  * m[7]  * m[14] +
+    m[13] * m[6]  * m[11] -
+    m[13] * m[7]  * m[10];
+    
+    inv[4] = -m[4]  * m[10] * m[15] +
+    m[4]  * m[11] * m[14] +
+    m[8]  * m[6]  * m[15] -
+    m[8]  * m[7]  * m[14] -
+    m[12] * m[6]  * m[11] +
+    m[12] * m[7]  * m[10];
+    
+    inv[8] = m[4]  * m[9] * m[15] -
+    m[4]  * m[11] * m[13] -
+    m[8]  * m[5] * m[15] +
+    m[8]  * m[7] * m[13] +
+    m[12] * m[5] * m[11] -
+    m[12] * m[7] * m[9];
+    
+    inv[12] = -m[4]  * m[9] * m[14] +
+    m[4]  * m[10] * m[13] +
+    m[8]  * m[5] * m[14] -
+    m[8]  * m[6] * m[13] -
+    m[12] * m[5] * m[10] +
+    m[12] * m[6] * m[9];
+    
+    inv[1] = -m[1]  * m[10] * m[15] +
+    m[1]  * m[11] * m[14] +
+    m[9]  * m[2] * m[15] -
+    m[9]  * m[3] * m[14] -
+    m[13] * m[2] * m[11] +
+    m[13] * m[3] * m[10];
+    
+    inv[5] = m[0]  * m[10] * m[15] -
+    m[0]  * m[11] * m[14] -
+    m[8]  * m[2] * m[15] +
+    m[8]  * m[3] * m[14] +
+    m[12] * m[2] * m[11] -
+    m[12] * m[3] * m[10];
+    
+    inv[9] = -m[0]  * m[9] * m[15] +
+    m[0]  * m[11] * m[13] +
+    m[8]  * m[1] * m[15] -
+    m[8]  * m[3] * m[13] -
+    m[12] * m[1] * m[11] +
+    m[12] * m[3] * m[9];
+    
+    inv[13] = m[0]  * m[9] * m[14] -
+    m[0]  * m[10] * m[13] -
+    m[8]  * m[1] * m[14] +
+    m[8]  * m[2] * m[13] +
+    m[12] * m[1] * m[10] -
+    m[12] * m[2] * m[9];
+    
+    inv[2] = m[1]  * m[6] * m[15] -
+    m[1]  * m[7] * m[14] -
+    m[5]  * m[2] * m[15] +
+    m[5]  * m[3] * m[14] +
+    m[13] * m[2] * m[7] -
+    m[13] * m[3] * m[6];
+    
+    inv[6] = -m[0]  * m[6] * m[15] +
+    m[0]  * m[7] * m[14] +
+    m[4]  * m[2] * m[15] -
+    m[4]  * m[3] * m[14] -
+    m[12] * m[2] * m[7] +
+    m[12] * m[3] * m[6];
+    
+    inv[10] = m[0]  * m[5] * m[15] -
+    m[0]  * m[7] * m[13] -
+    m[4]  * m[1] * m[15] +
+    m[4]  * m[3] * m[13] +
+    m[12] * m[1] * m[7] -
+    m[12] * m[3] * m[5];
+    
+    inv[14] = -m[0]  * m[5] * m[14] +
+    m[0]  * m[6] * m[13] +
+    m[4]  * m[1] * m[14] -
+    m[4]  * m[2] * m[13] -
+    m[12] * m[1] * m[6] +
+    m[12] * m[2] * m[5];
+    
+    inv[3] = -m[1] * m[6] * m[11] +
+    m[1] * m[7] * m[10] +
+    m[5] * m[2] * m[11] -
+    m[5] * m[3] * m[10] -
+    m[9] * m[2] * m[7] +
+    m[9] * m[3] * m[6];
+    
+    inv[7] = m[0] * m[6] * m[11] -
+    m[0] * m[7] * m[10] -
+    m[4] * m[2] * m[11] +
+    m[4] * m[3] * m[10] +
+    m[8] * m[2] * m[7] -
+    m[8] * m[3] * m[6];
+    
+    inv[11] = -m[0] * m[5] * m[11] +
+    m[0] * m[7] * m[9] +
+    m[4] * m[1] * m[11] -
+    m[4] * m[3] * m[9] -
+    m[8] * m[1] * m[7] +
+    m[8] * m[3] * m[5];
+    
+    inv[15] = m[0] * m[5] * m[10] -
+    m[0] * m[6] * m[9] -
+    m[4] * m[1] * m[10] +
+    m[4] * m[2] * m[9] +
+    m[8] * m[1] * m[6] -
+    m[8] * m[2] * m[5];
+    
+    det = m[0] * inv[0] + m[1] * inv[4] + m[2] * inv[8] + m[3] * inv[12];
+    
+    if (det == 0)
+        //return false;
+        assert(false);
+    
+    det = 1.0 / det;
+    
+    for (i = 0; i < 16; i++)
+        invOut[i] = inv[i] * det;
+    
+    // return true;
+    return Cc3dMatrix4(invOut);
 }
 Cc3dVector4 toV4(const Cc3dVector2&v2,float z,float w){
     return Cc3dVector4(v2.x(), v2.y(), z, w);
